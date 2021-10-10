246b2fc738a6de2924059c61147a063bc501e8f8
gacalyus <83091221+gacalyus@users.noreply.github.com>
2021-10-11 01:00:14 +0300

    son

--- src/pages/Aboutpage.js
+++ src/pages/Aboutpage.js
@@ -1,5 +1,18 @@
 const Aboutpage = () => {
-  return <div className="aboutpage">This is the Aboutpage</div>;
+  return <div className="aboutPage">
+    <h2>About the ones used in the project</h2>
+    <div className="aboutpageInfo">
+    <ul>
+      <li>https://www.tvmaze.com/api</li>
+      <li>React(router, Context api)</li>
+      <li>Sass</li>
+      <li>Shared on Github</li>
+      <li>Axios AJAX Request</li>
+      <li>Axios AJAX Request</li>
+      <li>Netlify was diployed to</li>
+    </ul>
+    </div>
+  </div>;
 };
 
 export default Aboutpage;
